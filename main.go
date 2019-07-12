3b411fcc667f68ba6daf2ea00cc2fc7e4e2a7d85
Guilherme Lima <guilhermelimaruby@gmail.com>
2019-07-12 12:54:37 -0300

    aula_1

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{"Tenis", "Confortável", 89, 3},
+		{"Fone", "Muito bom", 59, 2},
+		{"Produto novo", "Muito legal", 1.99, 1},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
+}
